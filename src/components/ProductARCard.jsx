c3d099cd4908b29c18c4c6fceeb4edd7cdf9e450
Muhammad Aqib <saadibhatti1921@gmail.com>
2025-08-24 11:25:17 +0500

    Update ProductARCard.jsx

--- src/components/ProductARCard.jsx
+++ src/components/ProductARCard.jsx
@@ -1,7 +1,7 @@
 export default function ProductARCard({ name, price, glbSrc, usdzSrc, poster }) {
   return (
     <div className="rounded-xl border p-4 bg-white shadow-sm">
-{/*      <model-viewer
+     <model-viewer
   src={glbSrc}
   ios-src={usdzSrc}
   poster={poster}
@@ -21,30 +21,8 @@ export default function ProductARCard({ name, price, glbSrc, usdzSrc, poster }) 
   >
     View in your space
   </button>
-</model-viewer> */}
-      <model-viewer
-  src="/models/sofa.glb"
-  ios-src="/models/sofa.usdz"
-  alt="3D Sofa"
-  ar
-  ar-modes="webxr scene-viewer quick-look"
-  ar-placement="floor"
-  ar-scale="auto:0.5 2"
-  style={{ width: "100%", height: "400px", borderRadius: "12px" }}
->
-  <button
-    slot="ar-button"
-    style={{
-      background: "black",
-      color: "white",
-      padding: "10px 16px",
-      borderRadius: "8px",
-      fontSize: "14px",
-    }}
-  >
-    View in your space
-  </button>
 </model-viewer>
+     
 
 
 
